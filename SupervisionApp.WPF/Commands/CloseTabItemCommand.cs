f819513c787ccc4b0412d67f11740ab400479aec
Дмитрий Амбурцев <dmess174@gmail.com>
2020-09-10 23:27:26 +0500

    Added check CanClosed Tab Item into CloseTabCommand

--- SupervisionApp.WPF/Commands/CloseTabItemCommand.cs
+++ SupervisionApp.WPF/Commands/CloseTabItemCommand.cs
@@ -1,15 +1,18 @@
 ﻿using SupervisionApp.WPF.Commands.Base;
 using SupervisionApp.WPF.Models.ViewModelNavigators;
+using SupervisionApp.WPF.ViewModels.TabItems;
 
 namespace SupervisionApp.WPF.Commands
 {
     public class CloseTabItemCommand : CommandBase
     {
         private readonly ITabItemViewModelNavigator _tabViewModelNavigator;
+        private readonly TabItemViewModelBase _tabItemViewModel;
 
-        public CloseTabItemCommand(ITabItemViewModelNavigator tabViewModelNavigator)
+        public CloseTabItemCommand(ITabItemViewModelNavigator tabViewModelNavigator, TabItemViewModelBase tabItemViewModel = null)
         {
             _tabViewModelNavigator = tabViewModelNavigator;
+            _tabItemViewModel = tabItemViewModel;
         }
 
         public override bool CanExecute(object parameter)
@@ -17,14 +20,17 @@ namespace SupervisionApp.WPF.Commands
             return _tabViewModelNavigator.CurrentTab != null;
         }
 
-        public override void Execute(object parameter) // TODO: Баг с закрытием не того окна
+        public override void Execute(object parameter)
         {
-            var index = _tabViewModelNavigator.TabItems.IndexOf(_tabViewModelNavigator.CurrentTab) - 1;
-            _tabViewModelNavigator.CloseTab(_tabViewModelNavigator.CurrentTab);
-            if (index == -1)
-                _tabViewModelNavigator.CurrentTab = _tabViewModelNavigator.TabItems[index + 1];
-            else
-                _tabViewModelNavigator.CurrentTab = _tabViewModelNavigator.TabItems[index];
+            var index = _tabViewModelNavigator.TabItems.IndexOf(_tabViewModelNavigator.CurrentTab);
+            if (_tabItemViewModel.CanClosed())
+            {
+                _tabViewModelNavigator.CloseTab(_tabViewModelNavigator.CurrentTab);
+                if (index < _tabViewModelNavigator.TabItems.Count && index != 0)
+                {
+                    _tabViewModelNavigator.CurrentTab = _tabViewModelNavigator.TabItems[index];
+                }
+            }
         }
     }
 }
